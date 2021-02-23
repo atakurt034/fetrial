36baee5e8635b2241da4740988b9ca1955d25cd5
atakurt034 <69969985+atakurt034@users.noreply.github.com>
2021-02-23 12:59:11 +0800

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <left>
+        <div className='steps'>
+          steps
+          <span className='active'>.</span>
+          <span>.</span>
+          <span>.</span>
+        </div>
+
+        <div className='body'>
+          <h2>Let's set up your account</h2>
+          <p>
+            Already have an account? <span>Sign in</span>
+          </p>
+          <form>
+            <input placeholder='Your name' type='text' />
+
+            <input placeholder='Email address' type='email' />
+
+            <select>
+              <option>I would describe my user type as</option>
+              <option>Programmer</option>
+              <option>Front-end</option>
+            </select>
+
+            <div>
+              <input type='password' id='password' placeholder='Password' />
+              <label htmlFor='password'>Minimum 8 characters</label>
+            </div>
+
+            <button disabled type='submit'>
+              Next
+            </button>
+            <p>
+              By click the "Next" button, you agree to creating a free account,
+              and to <span>Terms of Service</span> and{' '}
+              <span>Privacy Policy</span>{' '}
+            </p>
+          </form>
+        </div>
+      </left>
+
+      <right>
+        <div>
+          <h2>Dummy Heading</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            ejusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+      </right>
+    </main>
+  )
 }
 
-export default App;
+export default App
